de2116d6cceef05ae6ab9292cfaa8f5d5de76227
apolyansky <aleksey.polyansky@pladform.ru>
2021-10-26 21:29:46 +0300

    feature - homework done: Rendering the file structure as a tree

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Homework 2</h3>
+    <p class="filepath">filepath: {{path}}</p>
+    <ul>
+      <tree-list-item
+        v-for="(item, i) in items"
+        :key="`${item.name}_${i}`"
+        :id="`${item.name}_${i}`"
+        :item="item"
+        :path="`/${item.name}`"
+        :currentItem="currentItem"
+        @selectItem="onSelect"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TreeListItem from './components/TreeListItem/TreeListItem';
+import structure from '../public/static/node_modules.json'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TreeListItem,
+  },
+  data() {
+    return {
+      items: structure.contents,
+      currentItem: null,
+      path: '',
+    }
+  },
+  created() {
+    this.$root.$on('selectItem', ({id, path}) => {
+      this.onSelect(id)
+      this.path = path;
+    })
+  },
+  methods: {
+    onSelect(itemId) {
+      this.currentItem = itemId;
+    }
   }
 }
 </script>
@@ -23,6 +53,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 40px;
 }
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: flex;
+  align-items: baseline;
+  justify-content: flex-start;
+  flex-direction: column;
+  margin-bottom: 8px;
+  font-size: 24px;
+}
+
+.filepath {
+  text-align: left;
+  margin: 16px 0 24px;
+}
+
 </style>
